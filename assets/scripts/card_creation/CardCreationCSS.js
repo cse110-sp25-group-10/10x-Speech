eae4f1384d6c280b07abf87d75ddde1b0bbda8c3
ermge <rkung@ucsd.edu>
2025-05-14 19:40:48 -0700

    Change architecture and add HTML for a simple card creation form
    Create new folder assets
    Add basic web component for a card creation form

---
+++ assets/scripts/card_creation/CardCreationCSS.js
@@ -0,0 +1,9 @@
+export function getCardCreationCSS() {
+    // TODO: Add CSS for card creation form
+    return `
+        /* Placeholder */
+        input {
+            font-size: 1rem;
+        }
+    `;
+}
