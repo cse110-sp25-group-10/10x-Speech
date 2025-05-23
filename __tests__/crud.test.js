43dd55805f3557372b7ac78e0d35aa99d8931f21
ermge <rkung@ucsd.edu>
2025-05-22 23:15:21 -0700

    Reformat functions inside a JS object
    Reformat functions inside a JS object so that cards and decks can be created using on object constructor

--- __tests__/crud.test.js
+++ __tests__/crud.test.js
@@ -1,7 +1,7 @@
-import { createCard } from "../scripts/deck.js";
+import { Card } from "../scripts/deck.js";
 
 test("creates a JS object representing a card", () => {
-    expect(createCard("Title", "Description", 10)).toStrictEqual({
+    expect(Card("Title", "Description", 10)).toStrictEqual({
         "frontText": "Title",
         "backText": "Description",
         "time": 10,
@@ -9,5 +9,5 @@ test("creates a JS object representing a card", () => {
 });
 
 test("returns null for missing front text", () => {
-    expect(createCard("", "Lorem ipsum", 5)).toBe(null);
+    expect(Card("", "Lorem ipsum", 5)).toBe(null);
 });
