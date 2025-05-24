0c156c4c77cb9ed94ca5b67f73bbbc1ae8dc9880
JohnsonUCSD <joc036@ucsd.edu>
2025-05-24 01:46:36 -0700

    Added basic unit tests for readCard and deleteCard
    Probably need some more tests for these

--- __tests__/crud.test.js
+++ __tests__/crud.test.js
@@ -1,5 +1,6 @@
 import { createCard } from "../scripts/deck.js";
 
+// createCard tests
 test("creates a JS object representing a card", () => {
     expect(createCard("Valid front text", "Valid back text", 10)).toStrictEqual({
         "frontText": "Valid front text",
@@ -21,15 +22,15 @@ test("returns null for missing front text", () => {
 });
 
 test("returns null for over 60 character front text", () => {
-    expect(createCard("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", "Valid back text", 5)).toBe(null);
+    expect(createCard("a".repeat(61), "Valid back text", 5)).toBe(null);
 });
 
 test("returns null for missing back text", () => {
     expect(createCard("Valid front text", "", 5)).toBe(null);
 });
 
-test("returns null for over 60 character back text", () => {
-    expect(createCard("Valid front text", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", 5)).toBe(null);
+test("returns null for over 250 character back text", () => {
+    expect(createCard("Valid front text", "a".repeat(251), 5)).toBe(null);
 });
 
 test("returns null for non-number time", () => {
@@ -51,3 +52,59 @@ test("creates a JS object representing a card with only space as text", () => {
         "time": 30,
     });
 });
+
+// readCard tests
+test("returns card at index 2", () => {
+    const deck = new Deck();
+    deck.createCard("Example Topic1", "Example description1", 10);
+    deck.createCard("Example Topic2", "Example description2", 15);
+    deck.createCard("Example Topic3", "Example description3", 20);
+    expect(readCard(deck, 2)).toStrictEqual({
+        "frontText": "Example Topic3",
+        "backText": "Example description3",
+        "time": 20,
+    });
+});
+
+test("returns null for empty deck", () => {
+    const deck = new Deck();
+    expect(readCard(deck, 0)).toBe(null);
+});
+
+test("returns null for null deck", () => {
+    expect(readCard(null, 0)).toBe(null);
+});
+
+test("returns null for card outside of index", () => {
+    const deck = new Deck();
+    deck.createCard("Example Topic1", "Example description1", 10);
+    expect(readCard(deck, 2)).toStrictEqual(null);
+});
+
+// deleteCard tests
+test("updates the deck by deleting the card at index 1", () => {
+    const deck = new Deck();
+    deck.createCard("Example Topic1", "Example description1", 10);
+    deck.createCard("Example Topic2", "Example description2", 15);
+    deck.createCard("Example Topic3", "Example description3", 20);
+    const expected = new Deck();
+    deck.createCard("Example Topic1", "Example description1", 10);
+    deck.createCard("Example Topic3", "Example description3", 20);
+    deleteCard(deck, 1);
+    expect(deck).toStrictEqual(expected);
+});
+
+test("returns null for empty deck", () => {
+    const deck = new Deck();
+    expect(deleteCard(deck, 0)).toBe(null);
+});
+
+test("returns null for card outside of index", () => {
+    const deck = new Deck();
+    deck.createCard("Example Topic1", "Example description1", 10);
+    expect(deleteCard(deck, 2)).toStrictEqual(null);
+});
+
+// updateCard tests
+
+
