cba1f7471c85099a9c78b1cf85cce5da299ac479
JohnsonUCSD <joc036@ucsd.edu>
2025-06-07 00:50:49 -0700

    Added checks deleting deck and reloads after
    Deletes one of the preloaded decks and checks the remaining decks
    
    After, reloads the page and checks the remaining decks again.

--- __tests__/deletedeck.e2e.test.js
+++ __tests__/deletedeck.e2e.test.js
@@ -1,4 +1,4 @@
-describe('User creates a deck for the first time', () => {
+describe('User loads their decks and deletes one of them', () => {
     beforeAll(async () => {
         await page.goto('https://team10xflashcard.netlify.app/');
 
@@ -31,12 +31,78 @@ describe('User creates a deck for the first time', () => {
             });
         });
 
+        await page.reload();
+
     });
     
 
-    it('checking number of decks and the cards in the deck after reload', async () => {
-        console.log('Checking number of decks and the cards in the deck after reload');
+    it('checking that delete button is grayed out, then not when deck is selected', async () => {
+
+        await page.waitForSelector("existing-screen");
+
+        // checks that buttons are disabled
+        const deleteDisabled = await page.$eval('#delete-speech-button', (btn) => btn.disabled);
+        expect(deleteDisabled).toBe(true);
+        const editDisabled = await page.$eval('#edit-speech-button', (btn) => btn.disabled);
+        expect(editDisabled).toBe(true);
+        const studyDisabled = await page.$eval('#study-button', (btn) => btn.disabled);
+        expect(studyDisabled).toBe(true);
+
+        await page.click('deck-preview[data-deck-name="Tigers"]');
+
+        // checks that buttons are enabled
+        const deleteEnabled = await page.$eval('#delete-speech-button', (btn) => !btn.disabled);
+        expect(deleteEnabled).toBe(true);
+        const editEnabled = await page.$eval('#edit-speech-button', (btn) => !btn.disabled);
+        expect(editEnabled).toBe(true);
+        const studyEnabled = await page.$eval('#study-button', (btn) => !btn.disabled);
+        expect(studyEnabled).toBe(true);
+
+    }, 10000);
+
+    it('delete one of the decks', async () => {
+
+        await page.click('#delete-speech-button');
+
+        const decks = await page.evaluate(async () => {
+            const request = indexedDB.open('FlashcardAppDB');
+            return await new Promise((resolve) => {
+            request.onsuccess = () => {
+                const db = request.result;
+                const tx = db.transaction('decks', 'readonly');
+                const store = tx.objectStore('decks');
+                const getAll = store.getAll();
+                getAll.onsuccess = () => resolve(getAll.result);
+            };
+            });
+        });
+        expect(decks.length).toBe(1);
+        const deckNames = decks.map(d => d.deckName);
+        expect(deckNames).toContain('Whales');
+
+
+    }, 10000);
+
+    it('checks that deck is still deleted after reload ', async () => {
+
         await page.reload();
+
+        const decks = await page.evaluate(async () => {
+            const request = indexedDB.open('FlashcardAppDB');
+            return await new Promise((resolve) => {
+            request.onsuccess = () => {
+                const db = request.result;
+                const tx = db.transaction('decks', 'readonly');
+                const store = tx.objectStore('decks');
+                const getAll = store.getAll();
+                getAll.onsuccess = () => resolve(getAll.result);
+            };
+            });
+        });
+        expect(decks.length).toBe(1);
+        const deckNames = decks.map(d => d.deckName);
+        expect(deckNames).toContain('Whales');
+        
 
     }, 10000);
 
