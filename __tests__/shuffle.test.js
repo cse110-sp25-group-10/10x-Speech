763923cbd10dc59c9a711aa41ee13860c6932b71
JohnsonUCSD <joc036@ucsd.edu>
2025-06-07 22:19:26 -0700

    Created couple of basic tests for shuffleCard
    Shuffle for deck with 3 cards
    
    Shuffle for deck with 1 card
    
    Shuffle string (only suppsed to shuffle arrays)

---
+++ __tests__/shuffle.test.js
@@ -0,0 +1,38 @@
+import { Card, shuffleCards } from "../scripts/deck.js";
+import { Deck } from "../scripts/deck.js";
+
+// shuffleCards tests
+test("shuffled cards should still have the same cards as the original", () => {
+
+    const deck = Deck("Example Title");
+    const card1 = Card("Example Topic1", "Example description1");
+    const card2 = Card("Example Topic2", "Example description2");
+    const card3 = Card("Example Topic3", "Example description3");
+    deck.addCard(card1);
+    deck.addCard(card2);
+    deck.addCard(card3);
+
+    const shuffled = shuffleCards(deck.cards);
+
+    expect(shuffled.sort()).toStrictEqual(deck.cards.sort());
+});
+
+
+test("shuffling with single card deck shouldn't change anything", () => {
+
+    const deck = Deck("Example Title");
+    const card1 = Card("Example Topic1", "Example description1");
+    deck.addCard(card1);
+
+    const shuffled = shuffleCards(deck.cards);
+
+    expect(shuffled).toStrictEqual(deck.cards);
+});
+
+test("returns null for shuffling non-array objects", () => {
+
+    const deck = "this is not a deck";
+    const shuffled = shuffleCards(deck.cards);
+
+    expect(shuffled).toBe(null);
+});
