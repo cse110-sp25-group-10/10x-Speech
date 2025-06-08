2600909a1c8db4361a26a79eb394fdcdcc5e5c7b
JohnsonUCSD <joc036@ucsd.edu>
2025-06-07 22:27:26 -0700

    Added test with numbers and symbols

--- __tests__/shuffle.test.js
+++ __tests__/shuffle.test.js
@@ -1,9 +1,8 @@
-import { Card, shuffleCards } from "../scripts/deck.js";
-import { Deck } from "../scripts/deck.js";
+import { Card, Deck, shuffleCards } from "../scripts/deck.js";
 
 // shuffleCards tests
 test("shuffled cards should still have the same cards as the original", () => {
-
+    // creating deck
     const deck = Deck("Example Title");
     const card1 = Card("Example Topic1", "Example description1");
     const card2 = Card("Example Topic2", "Example description2");
@@ -17,9 +16,8 @@ test("shuffled cards should still have the same cards as the original", () => {
     expect(shuffled.sort()).toStrictEqual(deck.cards.sort());
 });
 
-
 test("shuffling with single card deck shouldn't change anything", () => {
-
+    // creating deck
     const deck = Deck("Example Title");
     const card1 = Card("Example Topic1", "Example description1");
     deck.addCard(card1);
@@ -30,7 +28,7 @@ test("shuffling with single card deck shouldn't change anything", () => {
 });
 
 test("returns null for shuffling non-array objects", () => {
-
+    // creating invalid deck
     const deck = "this is not a deck";
     const shuffled = shuffleCards(deck.cards);
 
@@ -38,9 +36,24 @@ test("returns null for shuffling non-array objects", () => {
 });
 
 test("shuffling empty deck should still work and keep deck empty", () => {
-
+    // creating deck
     const deck = Deck("Example Title");
     const shuffled = shuffleCards(deck.cards);
 
     expect(shuffled).toStrictEqual(deck.cards);
+});
+
+test("shuffled cards should work with letters, numberes, and symbols", () => {
+    // creating deck
+    const deck = Deck("R@nd0m ");
+    const card1 = Card("123456789", "aeiou");
+    const card2 = Card("%@!^&gshyis", "the front is gibberish");
+    const card3 = Card("xopa8*(89*", "some symbols and numbers and letters");
+    deck.addCard(card1);
+    deck.addCard(card2);
+    deck.addCard(card3);
+
+    const shuffled = shuffleCards(deck.cards);
+
+    expect(shuffled.sort()).toStrictEqual(deck.cards.sort());
 });
